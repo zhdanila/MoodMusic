7f423f0edbec19a83040f4d411f58f6be287fcec
Your Name <you@example.com>
2024-04-04 21:51:32 +0300

    Some optimization: '

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import pygame.mixer
 
 import audio_convert
 import indexFIle
-from window import add_text_and_image_about_us
+from window import add_text_and_image_about_us, add_audio_controls
 
 file_index = indexFIle.load_file_index()
 
@@ -148,60 +148,12 @@ def check_buttons_state():
         convert_button.config(state='disabled')
 
 
-def add_audio_controls():
-    global convert_button
-
-    style = ttk.Style()
-    style.configure('Custom.TButton', foreground='#000000', background='#5e34ab', font=('Helvetica', 12), padding=8)
-
-    file_label = tk.Label(change_audiofile, text="Виберіть MP3 файл:", font=('Helvetica', 14))
-    file_label.pack(pady=10)
-
-    select_button = ttk.Button(change_audiofile, text="Вибрати файл", command=select_file, style='Custom.TButton')
-    select_button.pack(pady=5)
-
-    mood_label = tk.Label(change_audiofile, text="Виберіть настрій:", font=('Helvetica', 14))
-    mood_label.pack(pady=10)
-
-    var.set(None)
-
-    radio_frame = tk.Frame(change_audiofile)
-    radio_frame.pack()
-    radio1 = tk.Radiobutton(radio_frame, image=image1, variable=var, value="sad", command=radio_selection)
-    radio2 = tk.Radiobutton(radio_frame, image=image2, variable=var, value="happy", command=radio_selection)
-    radio3 = tk.Radiobutton(radio_frame, image=image3, variable=var, value="confused", command=radio_selection)
-    radio4 = tk.Radiobutton(radio_frame, image=image4, variable=var, value="angry", command=radio_selection)
-
-    radio1.pack(side=tk.LEFT)
-    radio2.pack(side=tk.LEFT)
-    radio3.pack(side=tk.LEFT)
-    radio4.pack(side=tk.LEFT)
-
-    convert_button = ttk.Button(change_audiofile, text="Конвертувати", style='Custom.TButton', state='disabled',
-                                command=convert_audio)
-    convert_button.pack(pady=10)
-
-    button_frame = tk.Frame(change_audiofile)
-    button_frame.pack()
-
-    play_button = ttk.Button(button_frame, text="Відтворити", command=pygame.mixer.music.play, style='Custom.TButton',
-                             state='disabled')
-    play_button.pack(side=tk.LEFT, padx=5)
-
-    pause_button = ttk.Button(button_frame, text="Пауза", command=pause_music, style='Custom.TButton', state='disabled')
-    pause_button.pack(side=tk.LEFT, padx=5)
-
-    unpause_button = ttk.Button(button_frame, text="Продовжити", command=unpause_music, style='Custom.TButton',
-                                state='disabled')
-    unpause_button.pack(side=tk.LEFT, padx=5)
-
-    stop_button = ttk.Button(button_frame, text="Стоп", command=stop_music, style='Custom.TButton', state='disabled')
-    stop_button.pack(side=tk.LEFT, padx=5)
-
-    return play_button, pause_button, unpause_button, stop_button, convert_button
-
-
-play_button, pause_button, unpause_button, stop_button, convert_button = add_audio_controls()
+play_button, pause_button, unpause_button, stop_button, convert_button = add_audio_controls(change_audiofile, image1,
+                                                                                            image2, image3, image4,
+                                                                                            radio_selection,
+                                                                                            select_file, var,
+                                                                                            convert_audio, pause_music,
+                                                                                            unpause_music, stop_music)
 
 
 def add_to_taskbar():
