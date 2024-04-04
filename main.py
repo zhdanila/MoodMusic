a39fa9493154f1f7583faf9b8637dd578ad956a5
Your Name <you@example.com>
2024-04-04 13:22:50 +0300

    Delete unused comments

---
+++ main.py
@@ -0,0 +1,522 @@
+from tkinter import *
+from tkinter import ttk, filedialog
+import win32gui
+from pydub import AudioSegment
+from pedalboard import *
+from pedalboard.io import AudioFile
+import os
+import tkinter as tk
+import deezer
+import pygame
+import random
+import io
+import aiohttp
+import asyncio
+import pygame.mixer
+
+
+def load_file_index():
+    try:
+        with open('file_index.txt', 'r') as file:
+            return int(file.read())
+    except FileNotFoundError:
+        return 0
+
+
+file_index = load_file_index()
+
+
+def show_scene(scene_num):
+    for scene in scenes:
+        scene.pack_forget()
+    scenes[scene_num].pack()
+
+
+root = tk.Tk()
+root.title("MoodMusic")
+root.geometry("771x477")
+root.resizable(False, False)
+pygame.init()
+pygame.mixer.init()
+
+image1 = tk.PhotoImage(file="images/sad.png")
+image2 = tk.PhotoImage(file=r"images/happy.png")
+image3 = tk.PhotoImage(file=r"images/confused.png")
+image4 = tk.PhotoImage(file=r"images/angr.png")
+
+var = tk.StringVar(value="")
+
+top_frame = tk.Frame(root, bg="#5e34ab", height=100, width=100)
+top_frame.pack(side="top", fill="x")
+
+image_path = r"images/logo.png"
+image = tk.PhotoImage(file=image_path)
+image = image.subsample(3)
+logo_label = tk.Label(top_frame, image=image, bg="#5e34ab")
+logo_label.pack(expand=True)
+
+notebook = ttk.Notebook(style='Custom.TNotebook')
+notebook.pack(expand=True, fill=BOTH)
+
+about_us = ttk.Frame(notebook)
+melody_library = ttk.Frame(notebook)
+last_project = ttk.Frame(notebook)
+change_audiofile = ttk.Frame(notebook)
+
+change_audiofile.pack(fill=BOTH, expand=True)
+melody_library.pack(fill=BOTH, expand=True)
+last_project.pack(fill=BOTH, expand=True)
+about_us.pack(fill=BOTH, expand=True)
+
+notebook.add(change_audiofile, text="Змінити аудіофайл", compound=tk.LEFT)
+notebook.add(melody_library, text="Бібліотека мелодій", compound=tk.LEFT)
+notebook.add(last_project, text="Останні проєкти", compound=tk.LEFT)
+notebook.add(about_us, text="Про нас", compound=tk.LEFT)
+
+style = ttk.Style()
+style.configure('Custom.TNotebook.Tab', padding=[31, 8], anchor='center', focuscolor='#5e34ab')
+
+
+
+def add_text_and_image_about_us():
+    about_us_text = """
+    MoodMusic - це програма, яка автоматично підбирає та змінює музику з урахуванням настрою користувача, 
+    надаючи можливість створювати персональні плейлисти та налаштовувати музичний супровід у реальному часі 
+    для максимальної музичної гармонії зі своїми емоціями і потребами.
+    """
+    about_us_label = tk.Label(about_us, text=about_us_text, wraplength=600, font=("Helvetica", 14))
+    about_us_label.pack()
+
+
+    image_path = r"images/mood_two.png"
+    image = tk.PhotoImage(file=image_path)
+    resized_image = image.subsample(1)
+    image_label = tk.Label(about_us, image=resized_image)
+    image_label.image = resized_image
+    image_label.pack()
+
+    return resized_image
+
+
+resized_image = add_text_and_image_about_us()
+
+music_playing = False
+file_state = False
+
+
+def select_file():
+    global music_playing, convert_button
+    global file_path
+    file_path = filedialog.askopenfilename(filetypes=[("MP3 files", "*.mp3")])
+    if file_path:
+        global file_state
+        file_state = True
+        check_buttons_state()
+
+
+def save_file_index():
+    with open('file_index.txt', 'w') as file:
+        file.write(str(file_index))
+
+
+def pause_music():
+    if pygame.mixer.music.get_busy():
+        pygame.mixer.music.pause()
+
+
+def stop_music():
+    pygame.mixer.music.stop()
+
+
+def unpause_music():
+    if not pygame.mixer.music.get_busy():
+        pygame.mixer.music.unpause()
+
+
+def enable_buttons():
+    play_button.config(state='enabled')
+    unpause_button.config(state='enabled')
+    stop_button.config(state='enabled')
+    pause_button.config(state='enabled')
+
+
+def convert_audio():
+    global file_index
+    output_file = f"Project/result_{file_index}.mp3"
+
+    pygame.mixer.music.stop()
+    if (var.get() == "sad"):
+        audio = AudioSegment.from_file(file_path, format="mp3")
+
+        slowed_audio = audio._spawn(audio.raw_data, overrides={
+            "frame_rate": int(audio.frame_rate * 0.8)
+        })
+
+        slowed_audio.export(output_file, format="mp3")
+
+        samplerate = 44100.0
+        with AudioFile(output_file).resampled_to(samplerate) as f:
+            audio = f.read(f.frames)
+
+        os.remove(output_file)
+
+        board = Pedalboard([
+            Reverb(0.45, 0.5, 0.27, 0.35, 0.9, 0.0),
+            PitchShift(-2),
+        ])
+
+        file_index += 1
+        output_file = f"Project/project_{file_index}.mp3"
+
+        effected = board(audio, samplerate)
+
+        with AudioFile(output_file, 'w', samplerate, effected.shape[0]) as f:
+            f.write(effected)
+
+        pygame.mixer.music.load(output_file)
+        music_playing = False
+
+    elif (var.get() == "angry"):
+        audio = AudioSegment.from_file(file_path, format="mp3")
+
+        audio.export(output_file, format="mp3")
+
+        samplerate = 44100.0
+        with AudioFile(output_file).resampled_to(samplerate) as f:
+            audio = f.read(f.frames)
+
+        os.remove(output_file)
+
+        board = Pedalboard([
+            PitchShift(-8),
+            Distortion(10),
+            Delay(mix=0.2),
+            LowShelfFilter(cutoff_frequency_hz=600)
+        ])
+
+        file_index += 1
+        output_file = f"Project/project_{file_index}.mp3"
+
+        effected = board(audio, samplerate)
+
+        with AudioFile(output_file, 'w', samplerate, effected.shape[0]) as f:
+            f.write(effected)
+
+        pygame.mixer.music.load(output_file)
+        music_playing = False
+
+
+    elif (var.get() == "confused"):
+        audio = AudioSegment.from_file(file_path, format="mp3")
+
+        slowed_audio = audio._spawn(audio.raw_data, overrides={
+            "frame_rate": int(audio.frame_rate * 0.7)
+        })
+
+        slowed_audio.export(output_file, format="mp3")
+
+        samplerate = 44100.0
+        with AudioFile(output_file).resampled_to(samplerate) as f:
+            audio = f.read(f.frames)
+
+        os.remove(output_file)
+
+        board = Pedalboard([
+            Reverb(0.8),
+            PitchShift(-10),
+            Phaser(mix=0.2),
+            Distortion(0.1)
+        ])
+
+        file_index += 1
+        output_file = f"Project/project_{file_index}.mp3"
+
+        effected = board(audio, samplerate)
+
+        with AudioFile(output_file, 'w', samplerate, effected.shape[0]) as f:
+            f.write(effected)
+
+        pygame.mixer.music.load(output_file)
+        music_playing = False
+
+    elif (var.get() == "happy"):
+        audio = AudioSegment.from_file(file_path, format="mp3")
+
+        audio = audio.high_pass_filter(300)
+
+        audio = audio.speedup(1.1)
+
+        audio.export(output_file, format="mp3")
+
+        samplerate = 44100.0
+        with AudioFile(output_file).resampled_to(samplerate) as f:
+            audio = f.read(f.frames)
+
+        os.remove(output_file)
+
+        board = Pedalboard([
+            PitchShift(semitones=5),
+            Chorus(mix=0.2),
+            Phaser(mix=0.15)
+        ])
+
+        file_index += 1
+        output_file = f"Project/project_{file_index}.mp3"
+
+        effected = board(audio, samplerate)
+
+        with AudioFile(output_file, 'w', samplerate, effected.shape[0]) as f:
+            f.write(effected)
+
+        pygame.mixer.music.load(output_file)
+        music_playing = False
+
+    play_button.config(state='enabled')
+    pause_button.config(state='enabled')
+    unpause_button.config(state='enabled')
+    stop_button.config(state='enabled')
+
+    save_file_index()
+
+
+def radio_selection():
+    global convert_button
+
+    check_buttons_state()
+
+
+def check_buttons_state():
+    selected_value = var.get()
+    if selected_value != "None" and file_state:
+        convert_button.config(state='enabled')
+    else:
+        convert_button.config(state='disabled')
+
+
+def add_audio_controls():
+    global convert_button
+
+    style = ttk.Style()
+    style.configure('Custom.TButton', foreground='#000000', background='#5e34ab', font=('Helvetica', 12), padding=8)
+
+    file_label = tk.Label(change_audiofile, text="Виберіть MP3 файл:", font=('Helvetica', 14))
+    file_label.pack(pady=10)
+
+    select_button = ttk.Button(change_audiofile, text="Вибрати файл", command=select_file, style='Custom.TButton')
+    select_button.pack(pady=5)
+
+    mood_label = tk.Label(change_audiofile, text="Виберіть настрій:", font=('Helvetica', 14))
+    mood_label.pack(pady=10)
+
+    var.set(None)
+
+    radio_frame = tk.Frame(change_audiofile)
+    radio_frame.pack()
+    radio1 = tk.Radiobutton(radio_frame, image=image1, variable=var, value="sad", command=radio_selection)
+    radio2 = tk.Radiobutton(radio_frame, image=image2, variable=var, value="happy", command=radio_selection)
+    radio3 = tk.Radiobutton(radio_frame, image=image3, variable=var, value="confused", command=radio_selection)
+    radio4 = tk.Radiobutton(radio_frame, image=image4, variable=var, value="angry", command=radio_selection)
+
+    radio1.pack(side=tk.LEFT)
+    radio2.pack(side=tk.LEFT)
+    radio3.pack(side=tk.LEFT)
+    radio4.pack(side=tk.LEFT)
+
+    convert_button = ttk.Button(change_audiofile, text="Конвертувати", style='Custom.TButton', state='disabled',
+                                command=convert_audio)
+    convert_button.pack(pady=10)
+
+    button_frame = tk.Frame(change_audiofile)
+    button_frame.pack()
+
+    play_button = ttk.Button(button_frame, text="Відтворити", command=pygame.mixer.music.play, style='Custom.TButton',
+                             state='disabled')
+    play_button.pack(side=tk.LEFT, padx=5)
+
+    pause_button = ttk.Button(button_frame, text="Пауза", command=pause_music, style='Custom.TButton', state='disabled')
+    pause_button.pack(side=tk.LEFT, padx=5)
+
+    unpause_button = ttk.Button(button_frame, text="Продовжити", command=unpause_music, style='Custom.TButton',
+                                state='disabled')
+    unpause_button.pack(side=tk.LEFT, padx=5)
+
+    stop_button = ttk.Button(button_frame, text="Стоп", command=stop_music, style='Custom.TButton', state='disabled')
+    stop_button.pack(side=tk.LEFT, padx=5)
+
+    return play_button, pause_button, unpause_button, stop_button, convert_button
+
+
+play_button, pause_button, unpause_button, stop_button, convert_button = add_audio_controls()
+
+
+def add_to_taskbar():
+    hwnd = root.winfo_id()
+    win32gui.ShowWindow(hwnd, 5)
+    win32gui.SetParent(hwnd, 0)
+    root.update_idletasks()
+
+
+def update_list():
+    file_list.delete(0, tk.END)
+
+    global folder_path
+
+    folder_path = os.path.join("Project")
+
+    mp3_files = [file for file in os.listdir(folder_path) if file.endswith(".mp3")]
+
+    for file in mp3_files:
+        file_list.insert(tk.END, file)
+
+
+style.configure('Custom.TButton', foreground='#000000', background='#5e34ab', font=('Helvetica', 12), padding=8)
+
+update_button = ttk.Button(last_project, text="Оновити список", command=update_list, style='Custom.TButton')
+update_button.pack(pady=10)
+
+file_list = tk.Listbox(last_project)
+file_list.pack()
+
+
+def play_selected():
+    selected_file = file_list.get(file_list.curselection())
+    file_path = os.path.join(folder_path, selected_file)
+
+    pygame.mixer.music.load(file_path)
+
+
+select_button = ttk.Button(last_project, text="Обрати", command=play_selected, style='Custom.TButton')
+select_button.pack(pady=5)
+
+button_frame = tk.Frame(last_project)
+button_frame.pack()
+
+play_button_2 = ttk.Button(button_frame, text="Відтворити", command=pygame.mixer.music.play, style='Custom.TButton',
+                           state='enabled')
+play_button_2.pack(side=tk.LEFT, padx=5)
+
+pause_button_2 = ttk.Button(button_frame, text="Пауза", command=pause_music, style='Custom.TButton', state='enabled')
+pause_button_2.pack(side=tk.LEFT, padx=5)
+
+unpause_button_2 = ttk.Button(button_frame, text="Продовжити", command=unpause_music, style='Custom.TButton',
+                              state='enabled')
+unpause_button_2.pack(side=tk.LEFT, padx=5)
+
+stop_button_2 = ttk.Button(button_frame, text="Стоп", command=stop_music, style='Custom.TButton', state='enabled')
+stop_button_2.pack(side=tk.LEFT, padx=5)
+
+add_to_taskbar()
+
+client = deezer.Client(app_id='656531', app_secret='be52f4ba4f5feb9b2f5740650c5b4d1b')
+
+current_track_index = -1
+current_tracks = []
+
+paused = False
+
+
+def pause_track():
+    global paused
+    pygame.mixer.music.pause()
+    paused = True
+
+
+def resume_track():
+    global paused
+    pygame.mixer.music.unpause()
+    paused = False
+
+
+def replay_track():
+    pygame.mixer.music.rewind()
+    pygame.mixer.music.play()
+
+
+def stop_track():
+    pygame.mixer.music.stop()
+
+
+async def play_random_track():
+    global current_tracks, current_track_index
+    track_category = category_var.get()
+
+    results = client.search(track_category)
+
+    if results:
+        current_tracks = results
+        current_track_index = random.randint(0, len(results) - 1)
+
+        try:
+            await play_current_track()
+        except Exception as e:
+            print(f"Помилка при відтворенні треку: {e}")
+
+
+async def check_music_finished():
+    while pygame.mixer.music.get_busy():
+        await asyncio.sleep(0.1)
+    pygame.mixer.quit()
+
+
+async def play_current_track():
+    global current_track_index, current_tracks
+    if current_track_index != -1 and current_tracks:
+        track = current_tracks[current_track_index]
+        track_url = track.preview
+        track_title = track.title
+
+        print(f"Playing: {track_title}")
+
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(track_url) as response:
+                    audio_data = io.BytesIO(await response.read())
+
+                    pygame.mixer.init()
+                    pygame.mixer.music.load(audio_data)
+                    pygame.mixer.music.play()
+
+                    await asyncio.sleep(0.1)
+                    asyncio.create_task(check_music_finished())
+
+        except aiohttp.ClientError as e:
+            print(f"Помилка під час завантаження аудіо: {e}")
+
+
+async def start_play_random_track():
+    loop = asyncio.get_event_loop()
+    await loop.create_task(play_random_track())
+
+
+def play_next_track():
+    global current_track_index
+    if current_tracks:
+        current_track_index = (current_track_index + 1) % len(current_tracks)
+        asyncio.run(play_current_track())
+
+
+categories = ["relaxed", "workout", "concentration", "depressive", "energetic"]
+
+category_var = tk.StringVar(melody_library)
+category_var.set(categories[0])
+
+category_menu = tk.OptionMenu(melody_library, category_var, *categories)
+category_menu.config(font=("Helvetica", 14))
+category_menu.pack(pady=30)
+
+style = ttk.Style()
+
+style.configure('Custom.TButton', foreground='#000000', background='#5e34ab', font=('Helvetica', 12), padding=8)
+play_button_2 = ttk.Button(melody_library, text="Обрати категорію",
+                           command=lambda: root.after(0, lambda: asyncio.run(start_play_random_track())),
+                           style='Custom.TButton')
+play_button_2.pack(pady=5)
+
+next_button = ttk.Button(melody_library, text="Відтворити наступний трек", command=play_next_track,
+                         style='Custom.TButton')
+next_button.pack(pady=5)
+
+stop_button_3 = ttk.Button(melody_library, text="Зупинити", command=stop_music, style='Custom.TButton')
+stop_button_3.pack(pady=5)
+
+root.mainloop()
